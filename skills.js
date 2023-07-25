2692b78293eb26fc5c9b73c31ec6d913cd955444
Doug Inman <dougie@wwp.com.au>
2023-07-25 07:20:55 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,7 @@
+function calculateNumbers(var1, var2) {
+    // display to console the values of var1 and var2 and the result
+    console.log("var1: " + var1);
+    console.log("var2: " + var2);
+    console.log("result: " + (var1 + var2));
+    return var1 + var2;
+}
